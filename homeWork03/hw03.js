159838a943fbe7d791b0c553864f9c54f8fcfaab
MaxVolder <135257829+MaxVolder@users.noreply.github.com>
2023-06-17 20:29:43 +0300

    new0005

--- homeWork03/hw03.js
+++ homeWork03/hw03.js
@@ -13,7 +13,7 @@ function getMaxDigit(number) {
   
     return maxDigit;
   }
-  console.log(getMaxDigit(155673));
+  console.log(`Функція No1, яка виводить найбільшу цифру в числі 1236: ${getMaxDigit(1236)}`)
 
 //Задача 2
   function powerOfANumber(number, degree) {
@@ -23,13 +23,16 @@ function getMaxDigit(number) {
     }
     return result;
   }
-  console.log(powerOfANumber(3,3)); 
+  console.log(`Функція No2, яка виводить 3 в 3-ій ступені: ${powerOfANumber(3,3)}`); 
 
   // Задача 3
   function formatName(name) {
     const formattedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
     return formattedName;
   }
+  let name = "вОлоДимиР";
+  const formattedName = formatName(name);
+  console.log(`Функція No3, яка виводить форматоване імя вОлоДимиР: ${formatName(name)}`);
   
   //Задача 4
   function calculateNetIncome(grossIncome) {
@@ -40,13 +43,13 @@ function getMaxDigit(number) {
   
     return netIncome;
   }
-  
+  console.log(`Функція No4, яка виводить заробітню плату з суми 1000, після сплати податку в розмірі 19,5%: ${calculateNetIncome(1000)}`)
 // Задача 5
 function getRandomInteger(N, M) {
     const randomInteger = Math.floor(Math.random() * (M - N + 1)) + N;
     return randomInteger;
   }
-
+  console.log(`Функція No5, яка повертає випадкова ціле число в діапазоні між 1 та 699: ${getRandomInteger(1, 699)}`)
 //Задача 6
 function countLetterOccurrences(word, letter) {
     const lowercasedWord = word.toLowerCase();
@@ -61,6 +64,7 @@ function countLetterOccurrences(word, letter) {
   
     return count;
   }
+  console.log(`Функція No6, яка рахує скільки разів повторюється літера а в слові Абревіатура: ${countLetterOccurrences('Абревіатура', 'а')}`)
 // Задача 7
 function convertCurrency(currencyString) {
     const amount = parseFloat(currencyString);
@@ -76,7 +80,7 @@ function convertCurrency(currencyString) {
       return "Помилка!Невідома валюта";
     }
   }
-  
+  console.log(`Функція No7, яка конвертує 3700 грн в долар по курсу 37: ${convertCurrency('3700uah')}`)
   // Задача 8
   function getRandomPassword(length = 8) {
     let password = "";
@@ -90,6 +94,6 @@ function convertCurrency(currencyString) {
   
     return password;
   }
-  
+  console.log(`Функція No8, яка ствоює рандомний пароль довжиною 8 символів: ${getRandomPassword(length = 8)}`)
 
   

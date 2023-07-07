a0878d549481ba2a0783f265627c5385b7ab8fca
MaxVolder <135257829+MaxVolder@users.noreply.github.com>
2023-07-07 14:22:34 +0300

    hw09/0

--- homeWork08/hw08.js
+++ homeWork08/hw08.js
@@ -9,53 +9,68 @@ class Student {
   
     getInfo() {
       return `Студент ${this.course}го курсу ${this.university}, ${this.name}`;
-    }
-  
+    };
+
     get marks() {
       return this._marks;
-    }
+    };
   
     set marks(mark) {
-      if (!this.dismissed) {
-        this._marks.push(mark);
-      }
-    }
-  
+      this.marks === null ? null : this.marks.push(item)
+    };
+
     getAverageMark() {
       if (this._marks.length === 0) {
         return 0;
-      }
+      };
   
       const sum = this._marks.reduce((total, mark) => total + mark);
       return sum / this._marks.length;
-    }
+    };
   
     dismiss() {
       this.dismissed = true;
       this._marks = null;
-    }
+    };
   
     recover() {
       this.dismissed = false;
       this._marks = [];
-    }
-  }
+    };
+  };
+const student = new Student(1, "Вищої Школи Психотерапії м. Одеса", "Остап Родоманський Бендер");
+console.log(student.getInfo()); 
 
-  // Advance
-  class BudgetStudent extends Student {
-    constructor(course, university, name, isBudget) {
-      super(course, university, name, isBudget);
-      this.scholarshipTimer = setInterval(() => {
-        if (this.isBudget && !this.dismissed) {
-          this.getScholarship();
-        }
-      }, 30000);
-    }
+student.marks = 5;
+console.log(student.marks); 
   
-    getScholarship() {
-      if (this.getAverageMark() >= 4.0) {
-        console.log("Ви отримали 1400 грн. стипендії.");
+student.marks = 4;
+student.marks = 4;
+student.marks = 5;
+console.log(student.marks);
+console.log(student.getAverageMark());
+  
+student.dismiss();
+student.marks = 5;
+console.log(student.marks); // Виведе null
+  
+student.recover();
+student.marks = 5;
+console.log(student.marks); // Виведе [5]
+// Advance
+class BudgetStudent extends Student {
+  constructor(course, university, name, isBudget) {
+    super(course, university, name, isBudget);
+    this.scholarshipTimer = setInterval(() => {
+      if (this.isBudget && !this.dismissed) {
+        this.getScholarship();
       }
-    }
+    }, 30000);
   };
   
+  getScholarship() {
+    if (this.getAverageMark() >= 4.0) {
+      console.log("Ви отримали 1400 грн. стипендії.");
+    }
+  }
+};

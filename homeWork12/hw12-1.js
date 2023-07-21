eaa32e89fc08f2f1554f9c214347ef2407774b4c
MaxVolder <135257829+MaxVolder@users.noreply.github.com>
2023-07-21 14:29:39 +0300

    hw12

---
+++ homeWork12/hw12-1.js
@@ -0,0 +1,88 @@
+const getInfoButton = document.getElementById("getInfo");
+const nextButton = document.getElementById("next");
+const translateButton = document.getElementById("translate");
+
+let showingCharacters;
+
+getInfoButton.addEventListener("click", getInfo);
+nextButton.addEventListener("click", showAllPlanets);
+translateButton.addEventListener("click", translateButton);
+
+async function getInfo() {
+  try {
+    document.getElementById("getInfoOutput").innerHTML = "Loading...";
+    showingCharacters = true;
+    document.getElementById("getPlanetsOutput").innerHTML = "";
+    const episodeId = parseInt(document.getElementById("input").value);
+
+    if (episodeId >= 1 && episodeId <= 6) {
+      const resp = await fetch("https://swapi.dev/api/films/");
+      const data = await resp.json();
+      const episodeArr = data.results.filter(
+        (item) => item.episode_id === episodeId
+      );
+      const episode = episodeArr[0];
+      const dataArr = episode.characters;
+      let result = "";
+
+      for (const element of dataArr) {
+        const tempResp = await fetch(element);
+        const tempData = await tempResp.json();
+        result += `Ім'я персонажа: ${tempData.name}, рік народження: ${tempData.birth_year}, стать: ${tempData.gender} <br>`;
+      }
+
+      document.getElementById("getInfoOutput").innerHTML = result;
+      document.getElementById("getPlanetsOutput").innerHTML = "";
+    } else {
+      alert("Номер епізоду повинен бути від 1 до 6.");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function showAllPlanets() {
+  try {
+    document.getElementById("getPlanetsOutput").innerHTML =
+      "Loading planets...";
+    showingCharacters = false;
+    document.getElementById("getInfoOutput").innerHTML = "";
+    showResults(1);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function showResults(page) {
+  let result = "";
+  const resp = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
+  const data = await resp.json();
+  const dataArr = data.results;
+
+  for (const element of dataArr) {
+    result += `${element.name} <br>`;
+  }
+
+  document.getElementById("getPlanetsOutput").innerHTML = result;
+
+  if (page > 1) {
+    const backButton = document.createElement("button");
+    backButton.id = "back";
+    backButton.textContent = "Previous page";
+    document.getElementById("getPlanetsOutput").appendChild(backButton);
+
+    backButton.addEventListener("click", () => {
+      showResults(page - 1);
+    });
+  }
+  
+  if (page < 6) {
+    const nextButton = document.createElement("button");
+    nextButton.id = "next";
+    nextButton.textContent = "Next page";
+    document.getElementById("getPlanetsOutput").appendChild(nextButton);
+    nextButton.addEventListener("click", () => {
+      showResults(page + 1);
+    });
+  }
+}
